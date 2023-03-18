541f5e1866f0d0ee7e955305c7b9e6c1067e9c3f
Vinoth1923 <vinothvenkittu11@gmail.com>
2023-03-18 10:44:31 +0530

    message

---
+++ src/org/java/FibonacciSeries.java
@@ -0,0 +1,18 @@
+package org.java;
+
+public class FibonacciSeries {
+	
+	public static void main(String[] args) {
+		
+		int n1=0,n2=1,n3,i,count=10;
+		System.out.print(n1+" "+n2);
+		
+		for (int i1 = 2; i1 < count; ++i1) {
+			n3=n1+n2;
+			System.out.print(" "+n3);
+			n1=n2;
+			n2=n3;
+		}
+		
+	}
+}
---
+++ src/org/java/ReverseString.java
@@ -0,0 +1,14 @@
+package org.java;
+
+public class ReverseString {
+	
+	public static void main(String[] args) {
+		String s1="MATHEMATICS";
+		String rev="";
+		
+		for (int i = s1.length()-1; i>=0; i--) {
+			rev=rev+s1.charAt(i);
+		}
+			System.out.println(rev);	
+	}
+}
